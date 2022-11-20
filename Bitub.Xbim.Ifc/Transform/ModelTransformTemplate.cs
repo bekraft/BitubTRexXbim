04d3aa5e7ae4d7c9959dd539a255050fb08618f2
Bernold Kraft <bernold.kraft@gmail.com>
2022-11-20 16:27:14 +0100

    Fixing class cast exception on transform

--- Bitub.Xbim.Ifc/Transform/ModelTransformTemplate.cs
+++ Bitub.Xbim.Ifc/Transform/ModelTransformTemplate.cs
@@ -121,10 +121,10 @@ namespace Bitub.Xbim.Ifc.Transform
             return TransformResult.Code.Finished;
         }
 
-        protected IEnumerable<E> EmptyToNull<E>(IEnumerable<E> elements)
-        {
+        protected ExpressEnumerableWrapper<E> EmptyToNull<E>(IEnumerable<E> elements)
+        {           
             if (elements.Any())
-                return elements.ToArray();
+                return new ExpressEnumerableWrapper<E>(elements.ToArray());
             else
                 return null;
         }
