ab6bbd65145531a7ebf6945529768aea3ebbe789
Bernold Kraft <bernold.kraft@gmail.com>
2025-09-16 22:21:18 +0200

    BIM-177 New Ifc Relation Info

---
+++ Bitub.Xbim.Ifc/Transform/IfcRelationInfo.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+using Xbim.Common;
+using Xbim.Ifc4.Interfaces;
+
+namespace Bitub.Xbim.Ifc.Transform;
+
+/// <summary>
+/// Generic Ifc Relation Info interface which aggregates type of relation, target type and handling on host instances.
+/// </summary>
+public interface IIfcRelationInfo
+{
+    Type IfcRelationType { get; }
+    Type ParentType { get; }
+    Type TargetType { get; }
+    string Name { get; }
+    string HostPropertyName { get; }
+    string TargetPropertyName { get; }
+    IEnumerable<PropertyInfo> GetRelationInfoOf(IPersistEntity entity);
+    IEnumerable<IIfcRelationship> GetRelationOf(IPersistEntity entity);
+    IEnumerable<IPersistEntity> GetTargetOf(IPersistEntity entity);
+    IEnumerable<IPersistEntity> GetParentOf(IPersistEntity entity);
+}
+
+/// <summary>
+/// Concrete Ifc Relation Info implementation
+/// </summary>
+/// <param name="Name">The semantic name of relation info</param>
+/// <param name="HostPropertyName">The host's property name holding the reference to relation</param>
+/// <param name="TargetPropertyName">The relations target property name pointing towards the intended direction</param>
+/// <typeparam name="TRelation">The relation type</typeparam>
+/// <typeparam name="PValue">The parent value type</typeparam>
+/// <typeparam name="TValue">The target value type</typeparam>
+public record IfcRelationInfo<TRelation, PValue, TValue>(
+    string Name, 
+    string HostPropertyName,
+    string ParentPropertyName,
+    string TargetPropertyName) : IIfcRelationInfo where TRelation : IIfcRelationship 
+{
+    public Type IfcRelationType { get; } = typeof(TRelation);
+    public Type ParentType { get; } = typeof(PValue);
+    public Type TargetType { get; } = typeof(TValue);
+
+    /// <summary>
+    /// Returns the relation property info instances of given host entity.
+    /// </summary>
+    /// <param name="entity">The host entity</param>
+    /// <returns>An enumerable of relation property infos</returns>
+    public IEnumerable<PropertyInfo> GetRelationInfoOf(IPersistEntity entity)
+    {
+        return Enumerable.Concat(
+            // 1-ary relations
+            entity.GetType()
+                .GetLowerConstraintGenericProperty(IfcRelationType, null, HostPropertyName),
+            // n-ary relations
+            entity.GetType()
+                .GetLowerConstraintGenericProperty(typeof(IItemSet), IfcRelationType, HostPropertyName));
+    }
+
+    /// <summary>
+    /// Returns the relation objects
+    /// </summary>
+    /// <param name="entity">The host entity</param>
+    /// <returns>An enumerable of TRelation</returns>
+    public IEnumerable<TRelation> GetRelationOf(IPersistEntity entity)
+    {
+        return GetRelationInfoOf(entity)
+            .Select(i => i.GetValue(entity))
+            .Where(e => e != null)
+            .Cast<TRelation>();
+    }
+    
+    /// <summary>
+    /// Returns the target values of relation.
+    /// </summary>
+    /// <param name="entity">The host entity</param>
+    /// <returns>An enumerable of TValue</returns>
+    /// <exception cref="NotImplementedException">If property does not exist</exception>
+    public IEnumerable<TValue> GetTargetOf(IPersistEntity entity)
+    {
+        var targetPropertyInfo = IfcRelationType.GetProperty(TargetPropertyName);
+        if (null == targetPropertyInfo)
+            throw new NotImplementedException($"Target property {TargetPropertyName} does not exist on type {IfcRelationType.FullName}.");
+        
+        return GetRelationOf(entity)
+            .Select(r => targetPropertyInfo.GetValue(r))
+            .Where(e => e != null)
+            .Cast<TValue>();
+    }
+
+    /// <summary>
+    /// Returns the parent values of this relation.
+    /// </summary>
+    /// <param name="entity">The host entity</param>
+    /// <returns>An enumerable of PValue</returns>
+    /// <exception cref="NotImplementedException">If the parent property does not exist</exception>
+    public IEnumerable<PValue> GetParentOf(IPersistEntity entity)
+    {
+        var parentPropertyInfo = IfcRelationType.GetProperty(ParentPropertyName);
+        if (null == parentPropertyInfo)
+            throw new NotImplementedException($"Parent property {ParentPropertyName} does not exist on type {IfcRelationType.FullName}.");
+        
+        return GetRelationOf(entity)
+            .Select(r => parentPropertyInfo.GetValue(r))
+            .Where(e => e != null)
+            .Cast<PValue>();
+    }
+
+    #region IIfcRelationDef Members
+    
+    IEnumerable<IIfcRelationship> IIfcRelationInfo.GetRelationOf(IPersistEntity entity)
+    {
+        return GetRelationOf(entity).Cast<IIfcRelationship>();
+    }
+
+    IEnumerable<IPersistEntity> IIfcRelationInfo.GetTargetOf(IPersistEntity entity)
+    {
+        return GetTargetOf(entity).Cast<IPersistEntity>();
+    }
+
+    IEnumerable<IPersistEntity> IIfcRelationInfo.GetParentOf(IPersistEntity entity)
+    {
+        return GetParentOf(entity).Cast<IPersistEntity>();
+    }
+    
+    #endregion
+}
---
+++ Bitub.Xbim.Ifc/Transform/IfcRelationInstanceIndex.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xbim.Common;
+
+namespace Bitub.Xbim.Ifc.Transform;
+
+/// <summary>
+/// An Ifc relation visitor used by walking hierarchies
+/// </summary>
+public interface IIfcRelationVisitor
+{
+    /// <summary>
+    /// Visit an entity at given depth
+    /// </summary>
+    /// <param name="entity"></param>
+    /// <param name="level"></param>
+    /// <param name="relationInfo"></param>
+    /// <returns></returns>
+    bool Visit(IPersistEntity entity, int level, IIfcRelationInfo relationInfo);
+}
+
+/// <summary>
+/// An Ifc Relation hierarchical index.
+/// </summary>
+public class IfcRelationInstanceIndex
+{
+    /// <summary>
+    /// Parent - Instance Level record
+    /// </summary>
+    /// <param name="Parent">A parent</param>
+    /// <param name="Level">A level (starting with 0)</param>
+    struct NodeAtLevel(XbimInstanceHandle Parent, XbimInstanceHandle[] Children, int Level = 0)
+    {
+        public XbimInstanceHandle Parent { get; } =  Parent;
+        public int Level { get; set; } = Level;
+        public XbimInstanceHandle[] Children { get; set; } = Children;
+    }
+
+    #region Private Members
+
+    // Relation Info - Instance Handle - ParentInstanceAtLevel
+    private readonly Dictionary<IIfcRelationInfo, Dictionary<XbimInstanceHandle, NodeAtLevel>> _relationCache;
+    
+    #endregion
+
+    /// <summary>
+    /// New relation instance cache, register the given relations for tracking.
+    /// </summary>
+    /// <param name="ifcRelationInfo">The relation info to track</param>
+    public IfcRelationInstanceIndex(params IIfcRelationInfo[] ifcRelationInfo)
+    {
+        _relationCache = new ();
+        // Prepare internal cache
+        foreach (var relationInfo in ifcRelationInfo)
+        {
+            _relationCache[relationInfo] = new Dictionary<XbimInstanceHandle, NodeAtLevel>();
+        }
+    }
+
+    // Propagate level change 
+    private void PropagateLevelChange(Dictionary<XbimInstanceHandle, NodeAtLevel> cache, 
+        XbimInstanceHandle instance, int level)
+    {
+        var queue = new Queue<Tuple<XbimInstanceHandle, int>>();
+        queue.Enqueue(new Tuple<XbimInstanceHandle, int>(instance, level));
+        
+        while (queue.Count > 0)
+        {
+            var t = queue.Dequeue();
+            if (cache.TryGetValue(t.Item1, out NodeAtLevel nodeAtLevel))
+            {
+                nodeAtLevel.Level = t.Item2;
+                cache[t.Item1] = nodeAtLevel;    
+            }
+            
+            foreach(var childHandle in nodeAtLevel.Children)
+                queue.Enqueue(new Tuple<XbimInstanceHandle, int>(childHandle, t.Item2 + 1));
+        }
+    }
+    
+    /// <summary>
+    /// Puts an entity to hierarchy cache, if any registered relation matches.
+    /// </summary>
+    /// <param name="entity">The entity host</param>
+    /// <returns>True, if there's a match</returns>
+    /// <exception cref="NotSupportedException">Thrown, if there are more than 1 parents held by relation</exception>
+    public bool PutCache(IPersistEntity entity)
+    {
+        bool hasRelationType = false;
+        var instanceHandle = new XbimInstanceHandle(entity);
+        
+        foreach (var relationInfo in _relationCache.Keys)
+        {
+            var cache = _relationCache[relationInfo];
+            var parents = relationInfo
+                .GetParentOf(entity)
+                .ToArray();
+            
+            if (parents.Length > 1)
+            {
+                throw new NotSupportedException($"Only single rooted relations are allowed. Got: {parents.Length}");
+            }
+            else if (parents.Length == 1)
+            {
+                var parentHandle = new XbimInstanceHandle(parents.First());
+                var childHandles = relationInfo
+                    .GetTargetOf(entity)
+                    .Select(e => new XbimInstanceHandle(e))
+                    .ToArray();
+
+                if (cache.TryGetValue(parentHandle, out NodeAtLevel parentAtLevel))
+                {
+                    // Parent already exist
+                    var level = parentAtLevel.Level + 1;
+                    cache[instanceHandle] = new NodeAtLevel(parentHandle, childHandles, level);
+                    PropagateLevelChange(cache, instanceHandle, level);
+                }
+                else
+                {
+                    // Parent unknown, start at default level 0
+                    cache[instanceHandle] = new NodeAtLevel(parentHandle, childHandles);
+                    PropagateLevelChange(cache, instanceHandle, 0);
+                }
+                hasRelationType = true;
+            }
+        }
+        return hasRelationType;
+    }
+}
